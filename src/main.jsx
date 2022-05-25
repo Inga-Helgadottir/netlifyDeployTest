1a385dffddbf4702d324b6672d8af189419148a9
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-05-25 12:07:02 +0200

    code from other repo

--- src/main.jsx
+++ src/main.jsx
@@ -12,41 +12,30 @@ import ChangeUser from "./components/ChangeUser";
 import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <BrowserRouter>
-  //   <Routes>
-  //     <Route path="/" element={<App />}>
-  //       <Route path="seeCocktails" element={<Cocktails />}></Route>
-  //       <Route path="seeCocktail/:id" element={<CocktailRecipe />}></Route>
-  //       <Route path="alcoholUnits" element={<AlcoholUnits />}></Route>
-  //       <Route path="makeCocktail" element={<MakeCocktails />}></Route>
-  //       <Route path="seeAllUsers" element={<SeeAllUsers />}></Route>
-  //       <Route path="changeUser" element={<ChangeUser />}></Route>
-  //       <Route path="changeUser/:id" element={<ChangeUser />}></Route>
-  //     </Route>
-  //     <Route
-  //       // default for when the link is wrong
-  //       // like this http://localhost:3000/hjklhjklh
-  //       path="*"
-  //       element={
-  //         <main style={{ padding: "1rem" }}>
-  //           <h1>There's nothing here!</h1>
-  //           <p>
-  //             <a href="/">To go back click here!</a>
-  //           </p>
-  //         </main>
-  //       }
-  //     />
-  //   </Routes>
-  // </BrowserRouter>
-  <HashRouter>
-    <Switch>
-      <Route path="seeCocktails" element={<Cocktails />}></Route>
-      <Route path="seeCocktail/:id" element={<CocktailRecipe />}></Route>
-      <Route path="alcoholUnits" element={<AlcoholUnits />}></Route>
-      <Route path="makeCocktail" element={<MakeCocktails />}></Route>
-      <Route path="seeAllUsers" element={<SeeAllUsers />}></Route>
-      <Route path="changeUser" element={<ChangeUser />}></Route>
-      <Route path="changeUser/:id" element={<ChangeUser />}></Route>
-    </Switch>
-  </HashRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="seeCocktails" element={<Cocktails />}></Route>
+        <Route path="seeCocktail/:id" element={<CocktailRecipe />}></Route>
+        <Route path="alcoholUnits" element={<AlcoholUnits />}></Route>
+        <Route path="makeCocktail" element={<MakeCocktails />}></Route>
+        <Route path="seeAllUsers" element={<SeeAllUsers />}></Route>
+        <Route path="changeUser" element={<ChangeUser />}></Route>
+        <Route path="changeUser/:id" element={<ChangeUser />}></Route>
+      </Route>
+      <Route
+        // default for when the link is wrong
+        // like this http://localhost:3000/hjklhjklh
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <h1>There's nothing here!</h1>
+            <p>
+              <a href="/">To go back click here!</a>
+            </p>
+          </main>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
 );
