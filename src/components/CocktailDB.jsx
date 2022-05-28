309078dfebc44b9d15d8a196603f2973573ee6fe
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-05-28 12:14:05 +0200

    fixed error

--- src/components/CocktailDB.jsx
+++ src/components/CocktailDB.jsx
@@ -25,10 +25,9 @@ export const CocktailDB = ({ props }) => {
     let afterSplitImg = img.split(".");
     let lastSplit = afterSplit[0].split("/");
     let lastSplitImg = afterSplitImg[0].split("/");
-    if (lastSplit.length === 4) {
-      if (lastSplit[3] === lastSplitImg[3]) {
-        return img;
-      }
+
+    if (lastSplit[3] === lastSplitImg[3]) {
+      return img;
     } else if (lastSplit[2] === lastSplitImg[3]) {
       return img;
     }
